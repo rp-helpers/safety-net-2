b152ff8c0511b7a180e4fa94c189586b39f5f73a
Rafał Pryjda <r.pryjda@op.pl>
2019-06-09 20:46:04 +0200

    safety-net-java

---
+++ src/test/java/com/example/sn/sn/SnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sn.sn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SnApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
